03b2f96c0dda84908f5fac20d7b8f8388a7a6115
Ilia Sevostianov <ilia.sevostianov@atom.team>
2025-03-26 11:34:34 +0300

    add filter to close windows

--- close_all_open_windows.sh
+++ close_all_open_windows.sh
@@ -1,1 +1,6 @@
-wmctrl -l | awk '{ print $1 }' | xargs -I{} wmctrl -ic {}
+FILTER=$1
+if [ -z "$FILTER" ]; then
+  FILTER=""
+fi
+
+wmctrl -l | grep $FILTER | grep -ve "restore-workspaces-ubuntu" |  awk '{ print $1 }' | xargs -I{} wmctrl -ic {}
